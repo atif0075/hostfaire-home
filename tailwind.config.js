53b014bbe85a7aa5e32ff859906287a141fed907
M Atif <chatif476@gmail.com>
2023-01-16 16:42:21 +0500

    build(deps): update package.json, tailwind.config.js and yarn.lock

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,11 @@ module.exports = {
     "./nuxt.config.{js,ts}",
     "./app.vue",
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#1f2937",
-          secondary: "#F000B8",
-          accent: "#37CDBE",
-          neutral: "#3D4451",
-          "base-100": "#FFFFFF",
-          info: "#3ABFF8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#F87272",
-        },
-      },
-    ],
-  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1f2937",
+      },
+    },
   },
-  plugins: [require("daisyui")],
 };
