86f0658eeb8f2defbef81a3672182fe8c7067905
M Atif <chatif476@gmail.com>
2023-01-16 20:39:25 +0500

    build(deps): update nuxt.config.ts, package.json, pages/index.vue and yarn.lock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    modules: [
+        '@nuxtjs/device',
+    ],
     build: {
         transpile: ['@animxyz/core', '@animxyz/vue3', 'clsx'],
     },
