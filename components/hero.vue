767780b56a38b5869a82ddb6ba4d4c151099edf8
M Atif <chatif476@gmail.com>
2023-01-16 22:50:54 +0500

    update hero.vue

--- components/hero.vue
+++ components/hero.vue
@@ -155,7 +155,7 @@
                 <div>
                   <div>
                     <img
-                      v-motion-fade
+                    v-motion-slide-visible-once-right
                       src="../assets/svg/hero.svg"
                       class="relative ml-auto"
                       alt=""
