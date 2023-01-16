64316ed15529b31b179206426e4cfe3f5a97f6f1
M Atif <chatif476@gmail.com>
2023-01-16 20:59:41 +0500

    update cursor.vue

--- components/cursor.vue
+++ components/cursor.vue
@@ -1,44 +1,47 @@
 <script setup lang="ts">
-onMounted(() => {
+const { isMobileOrTablet } = useDevice();
+let mouseHandler = () => {
   const coords = { x: 0, y: 0 };
   const circles = document.querySelectorAll(".circle");
-
   circles.forEach(function (circle: any, index: any) {
     circle.x = 0;
     circle.y = 0;
   });
-
   window.addEventListener("mousemove", function (e) {
     coords.x = e.clientX;
     coords.y = e.clientY;
   });
-
   function animateCircles() {
     let x = coords.x;
     let y = coords.y;
-
     circles.forEach(function (circle: any, index: any) {
       circle.style.left = x - 12 + "px";
       circle.style.top = y - 12 + "px";
-
       circle.style.scale = (circles.length - index) / circles.length;
-
       circle.x = x;
       circle.y = y;
-
       const nextCircle: any = circles[index + 1] || circles[0];
       x += (nextCircle.x - x) * 0.3;
       y += (nextCircle.y - y) * 0.3;
     });
-
     requestAnimationFrame(animateCircles);
   }
-
   animateCircles();
+};
+onMounted(() => {
+  if (isMobileOrTablet) {
+    let circles = document.querySelectorAll(".circle") as any;
+    circles.forEach(function (circle: any, index: any) {
+      circle.style.display = "none";
+    });
+    return;
+  } else {
+    mouseHandler();
+  }
 });
 </script>
 <template>
-  <main class="cursor">
+  <main :class="$device.isMobileOrTablet ? 'hidden' : ''" class="cursor">
     <div class="circle"></div>
     <div class="circle"></div>
     <div class="circle"></div>
